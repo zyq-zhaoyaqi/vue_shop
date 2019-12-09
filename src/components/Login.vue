4d9b55a2fc09e66ef78cda10a23969393858ad76
zhaoyaqi <1084606924@qq.com>
2019-12-09 21:39:58 +0800

    first commit

---
+++ src/components/Login.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="login_gen">
+      <div class="login_box">
+          <div class="avator_box"></div>
+      </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+}
+</script>
+
+<style>
+.login_gen{
+    background: #2b4b6b;
+    height: 100%;
+    position: relative;
+}
+.login_box{
+    width: 450px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 10px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%)
+}
+</style>
